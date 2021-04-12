a2f2707b2ece9fae1fe9b59878de238030bad9f6
zeta0134 <zeta0134@users.noreply.github.com>
2021-04-11 21:18:35 -0400

    Add tables for DPCM playback rate and repitching
    The repitching equivalency can be used to take a source note and play it at a lower pitch without needing to re-encode a copy there. One downside is the lower quality, which means some noise inherent in DPCM becomes more audible, so it's a tradeoff.

--- dpcm.py
+++ dpcm.py
@@ -52,3 +52,38 @@ def to_dpcm(pcm_samples):
       dpcm_bits.append(0)
       current_dpcm_level -= 2
   return pack_dpcm_bits_into_bytes(dpcm_bits)
+
+playback_rate = [None] * 16
+playback_rate[0x0] = 4181.71
+playback_rate[0x1] = 4709.93
+playback_rate[0x2] = 5264.04
+playback_rate[0x3] = 5593.04
+playback_rate[0x4] = 6257.95
+playback_rate[0x5] = 7046.35
+playback_rate[0x6] = 7919.35
+playback_rate[0x7] = 8363.42
+playback_rate[0x8] = 9419.86
+playback_rate[0x9] = 11186.1
+playback_rate[0xA] = 12604.0
+playback_rate[0xB] = 13982.6
+playback_rate[0xC] = 16884.6
+playback_rate[0xD] = 21306.8
+playback_rate[0xE] = 24858.0
+playback_rate[0xF] = 33143.9
+
+equivalency = [None] * 16
+equivalency[0xE] = midi.note_index("C4") - midi.note_index("G3")
+equivalency[0xD] = midi.note_index("G3") - midi.note_index("E3")
+equivalency[0xC] = midi.note_index("E3") - midi.note_index("C3")
+equivalency[0xB] = midi.note_index("C3") - midi.note_index("A2")
+equivalency[0xA] = midi.note_index("A2") - midi.note_index("G2")
+equivalency[0x9] = midi.note_index("G2") - midi.note_index("F2")
+equivalency[0x8] = midi.note_index("F2") - midi.note_index("D2")
+equivalency[0x7] = midi.note_index("D2") - midi.note_index("C2")
+equivalency[0x6] = midi.note_index("C2") - midi.note_index("B1")
+equivalency[0x5] = midi.note_index("B1") - midi.note_index("A1")
+equivalency[0x4] = midi.note_index("A1") - midi.note_index("G1")
+equivalency[0x3] = midi.note_index("G1") - midi.note_index("F1")
+equivalency[0x2] = midi.note_index("F1") - midi.note_index("E1")
+equivalency[0x1] = midi.note_index("E1") - midi.note_index("D1")
+equivalency[0x0] = midi.note_index("D1") - midi.note_index("C1")
