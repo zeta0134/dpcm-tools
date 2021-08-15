12a0d5de1f036fba68bfbfa8a6e2e3f1e8178066
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-08-14 23:52:44 -0400

    PoC: one repitched sample, get

--- repitcher.py
+++ repitcher.py
@@ -5,6 +5,7 @@ import fti
 import midi
 
 # python stdlib
+import io
 import wave
 import struct
 
@@ -25,7 +26,7 @@ def fix_sample_width(mono_frames, sample_width):
         min_sample = -32768
         max_sample = 32767
     scale = (max_sample - min_sample) / 127
-    scaled_frames = [int((sample - min_sample) / scale) for sample in mono_frames]
+    scaled_frames = [(sample - min_sample) / scale for sample in mono_frames]
     return scaled_frames
 
 def read_wave(filename):
@@ -43,6 +44,17 @@ def read_wave(filename):
     data = fix_sample_width(data, sample_width)
     return data, samplerate
     
+def samplerate_conversion_speed(source_rate, target_rate):
+    return source_rate / target_rate
+
+# quite possibly the worst quality. Aliasing abounds, but it's quick
+# and *probably* quieter than DPCM noise, so... maybe it's fine?
+def resample_nearest(source_samples, speed):
+    resampled_samples = []
+    new_length = int(len(source_samples) / speed)
+    for i in range(0, new_length):
+        resampled_samples.append(source_samples[int(i * speed)])
+    return resampled_samples
 
 def main():
     print("Will do a thing")
@@ -53,6 +65,29 @@ def main():
 
     data, samplerate = read_wave(filename)
     print("Read {} samples from {} at {} Hz".format(len(data), filename, samplerate))
+
+    target_rate = dpcm.playback_rate[quality]
+    conversion_speed = samplerate / target_rate
+    print("Will sample at {:.4g} speed to target {} Hz".format(conversion_speed, target_rate))
+    source_frequency = midi.frequency[midi.note_index(source_note)]
+    target_frequency = midi.frequency[midi.note_index(target_note)]
+    repitch_speed = target_frequency / source_frequency
+    print("To repitch from {} to {}, will adjust speed by {:.4g}%".format(source_note, target_note, repitch_speed))
+    combined_speed = conversion_speed * repitch_speed
+    print("Combined adjustment: {:.4g}%".format(combined_speed))
+
+    resampled_data = resample_nearest(data, combined_speed)
+    print("Resampled data to new length: {}".format((len(resampled_data))))
+
+    if len(resampled_data) > 4081 * 8:
+        print("Data is too long, truncating to 4081*8 samples")
+        resampled_data = resampled_data[0:(4081*8)]
+
+    # sure, let's just spit it out
+    dpcm_data = dpcm.to_dpcm(resampled_data)
+    output = io.open("abass-D3.dmc", "wb")
+    output.write(dpcm_data)
+    output.close()
 
 
 
